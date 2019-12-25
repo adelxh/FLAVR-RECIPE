8bac3de9ae37d87cb64ce55b66239ab961948212
Adel Akhmed <adelxh@gmail.com>
2019-12-25 14:47:07 -0500

    Created file

---
+++ FLAVR-RECIPE/FLAVR-RECIPE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FLAVR-RECIPE
+//
+//  Created by Adel Akhmed on 2019-12-25.
+//  Copyright © 2019 Adel Akhmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
